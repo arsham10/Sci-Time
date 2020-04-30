cea282a1a886d9d280711bc5dbe353e495ddd32d
arsham10 <kidosol2256@gmail.com>
2020-04-30 15:31:28 +0800

    AI following complete
    AI following works but it doesnt have a range of sight

--- Assets/Scripts/EnemyGFX.cs
+++ Assets/Scripts/EnemyGFX.cs
@@ -13,11 +13,11 @@ public class EnemyGFX : MonoBehaviour
     {
 		if (aiPath.desiredVelocity.x >= 0.01f)
 		{
-			transform.localScale = new Vector3(-5f, 5f, 5f);
+			transform.localScale = new Vector3(5f, 5f, 5f);
 
 		}
 		else if (aiPath.desiredVelocity.x <= -0.01f) {
-			transform.localScale = new Vector3(5f,5f, 5f);
+			transform.localScale = new Vector3(-5f,5f, 5f);
 		}
     }
 }
